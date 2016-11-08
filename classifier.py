3c7a80ce679c2545f45d65bed6c27d27ef9eba1d
Justin Ting <justingling@gmail.com>
2016-11-08 20:51:10 +1100

    incorrect images, wip filtering image features

--- classifier.py
+++ classifier.py
@@ -4,11 +4,32 @@ from scipy import misc
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 
-path = 'correct_imgs/'
-imgs = []
-for f in listdir('correct_imgs/'):
-    imgs.append(misc.imread(path+f))
+def img_features_from_dir(path):
+    imgs = []
+    for f in listdir(path):
+        imgs.append(misc.imread(path+f))
 
-imgs = np.array(imgs)
-imgs = imgs.reshape(imgs.shape[0], -1)
+    return np.array(imgs)
+
+def gen_img_and_labels(imgs, label):
+    return imgs.reshape(imgs.shape[0], -1), np.full(imgs.shape[0], label, dtype=np.int64)
+
+def reduce_img_features(imgs, skip):
+    return imgs[:,::skip]
+
+# pull features of images
+correct_imgs = img_features_from_dir('correct_imgs/')
+incorrect_imgs = img_features_from_dir('incorrect_imgs/')
+
+# flatten and assign labels
+correct_imgs, correct_labels = gen_img_and_labels(correct_imgs, 1)
+incorrect_imgs, incorrect_labels = gen_img_and_labels(incorrect_imgs, 0)
+
+# merge image lists
+imgs = np.concatenate((correct_imgs, incorrect_imgs))
+imgs = reduce_img_features(imgs, 5)
+labels = np.concatenate((correct_labels, incorrect_labels))
+
 knn = KNeighborsClassifier(1)
+knn.fit(imgs, labels)
+
