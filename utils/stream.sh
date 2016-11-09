ea971d04a9030ca00c979dc00a386040ff914581
Justin Ting <justingling@gmail.com>
2016-11-09 14:26:40 +1100

    rerun stream on fail (certain hours of day may disable the stream)

--- utils/stream.sh
+++ utils/stream.sh
@@ -1,4 +1,7 @@
 stream_url=https://sevenwestmedia01-i.akamaihd.net/hls/live/224814/SYD1/master_high.m3u8
-date=`date +%Y-%m-%d`
-# ffmpeg -i "$stream_url" -c copy "$output_file"
-ffmpeg -i "$stream_url" -vf fps=1/2 imgs/$date-img%d.png
+
+while true
+do
+    date=`date +%Y-%m-%d`
+    ffmpeg -i "$stream_url" -vf fps=1/2 imgs/$date-img%d.png
+done
